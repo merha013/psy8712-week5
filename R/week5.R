4997afe58992e347bd075ee06ee8521b8624dc1f
Toni Merhar <merha013@umn.edu>
2024-02-15 22:42:02 -0600

    Still have several issues that need to be fixed but wanted a saved version with unpiped code that works in case I screw something up.

--- R/week5.R
+++ R/week5.R
@@ -9,8 +9,19 @@ Bdata_tbl <- read_tsv("../data/Bparticipants.dat", col_names = c("casenum", "par
 Bnotes_tbl <- read_tsv("../data/Bnotes.txt")
 
 # Data Cleaning
-# Aclean_tbl <- Adata_tbl %>%
-separate(Adata_tbl, qs, into=paste0("q",1:5))
-  # datadate <- mdy_hms(datadate) %>%  # still need to figure out how to do this part inside a pipe
-  # sapply(Adata_tbl[,paste0("q", 1:5)], as.integer)
+Aclean_tbl <- Adata_tbl %>%
+  separate(qs, into=paste0("q",1:5)) %>%
+  left_join(Anotes_tbl, by = "parnum") %>%
+  filter(is.na(notes))
 
+Aclean_tbl$datadate <- mdy_hms(Aclean_tbl$datadate) # needs to go in line 14
+Aclean_tbl[,paste0("q",1:5)] <- sapply(Aclean_tbl[,paste0("q", 1:5)], as.integer) # needs to go in line 15
+
+# this next section needs to begin on line 18
+ABclean_tbl <- Bdata_tbl %>%
+  left_join(Bnotes_tbl, by = "parnum") %>%
+  filter(is.na(notes))
+  # inner_join(Aclean_tbl)
+
+ABclean_tbl$datadate <- mdy_hms(ABclean_tbl$datadate) # needs to go in line 19
+ABclean_tbl[,5:14] <- sapply(Aclean_tbl[,5:14], as.integer) # needs to go in line 20, and needs to use other code than 5:14
