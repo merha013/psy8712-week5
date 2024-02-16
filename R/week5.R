c76fa0a92cb9e84ee063796a7f06b856ce63dcbd
Toni Merhar <merha013@umn.edu>
2024-02-16 11:21:58 -0600

    Figured out how to mutate as.integer. Aclean_tbl should be gtg but still working on ABclean_tbl. Then need to double check everything.

--- R/week5.R
+++ R/week5.R
@@ -12,17 +12,12 @@ Bnotes_tbl <- read_tsv("../data/Bnotes.txt")
 Aclean_tbl <- Adata_tbl %>%
   separate(qs, into=paste0("q",1:5)) %>%
   mutate(datadate = mdy_hms(datadate)) %>%
-  mutate(sapply(FUN = as.integer, paste0("q",1:5))) # doesn't work
+  mutate(across(!c("casenum", "parnum", "stimver", "datadate"), as.integer)) %>%
   left_join(Anotes_tbl, by = "parnum") %>%
   filter(is.na(notes))
-
-Aclean_tbl[,paste0("q",1:5)] <- sapply(Aclean_tbl[,paste0("q", 1:5)], as.integer) # needs to go in line 15
-
-# this next section needs to begin on line 18
 ABclean_tbl <- Bdata_tbl %>%
   mutate(datadate = mdy_hms(datadate)) %>%
+  mutate(across(!c("casenum", "parnum", "stimver", "datadate"), as.integer)) %>%
   left_join(Bnotes_tbl, by = "parnum") %>%
   filter(is.na(notes))
   # inner_join(Aclean_tbl)
-
-ABclean_tbl[,5:14] <- sapply(Aclean_tbl[,5:14], as.integer) # needs to go in line 20, and needs to use other code than 5:14
